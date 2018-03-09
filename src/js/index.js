32c4bac5eaa75de768daef959d4fdfabe4be80b9
Chaoyue77 <1337843513qq.com>
2018-03-09 20:58:47 +0800

    index.html完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 require(["config"], function(){
 	require(["jquery", "template", "load"], function($, template){
 		$.getJSON("/mock/list.json", function(data){
-			data = {list : data.res_body.data};
-			let html = template("list_template", data);
-			$(".list").html(html);
+			var data1 = {list : data.res_body.data};
+			var facedata = {list : data.res_body.facedata};
+			let html = template("list_template", data1),
+				facehtml = template("face_template", facedata);
+			$(".kh-products").html(html);
+			$(".face-products").html(facehtml);
 		});
+		
 	});
 });
--- src/js/loadHeaderFooter.js
+++ src/js/loadHeaderFooter.js
@@ -1,7 +1,7 @@
 define(["jquery", "cookie"], function() {   //定义模块
 	$("header").load("/html/include/header.html", function(){
 		/* 绑定搜索键盘事件 */
-		$(".search .word").on("keyup", function(){
+		$(".search :text").on("keyup", function(){
 			// jsonp 接口
 		let val = $(this).val(),
 			url = `https://suggest.taobao.com/sug?code=utf-8&q=${val}&callback=?`;
@@ -10,22 +10,41 @@ define(["jquery", "cookie"], function() {   //定义模块
 				url : url,
 				dataType : "jsonp",
 				success : function(data){
+					// console.log(data);
 					let html = "";
 					data.result.forEach(function(curr){
 						html += `<div>${curr[0]}</div>`;
 					});
-					$(".suggest").html(html);
+					$(".suggest_info").html(html);
 				}
 			});
 		});
+
+		//阻止事件冒泡，点击除了搜索框的其他部分下拉菜单消失
+		$(".search").click(function(event){
+			event.stopPropagation();    
+		});
+
+		$("body").on("click",$("body").not(".search"),function(){
+			$(".suggest_info").empty();
+		});
+	//选择菜单项，显示到搜索框中
+		$(".suggest_info").on("click", "div", function(){
+			$(".search :text").val($(this).text());
+			$(".suggest_info").empty();
+		})
 
 		/* 查询是否有登录用户 */
 		let user = $.cookie("loginUser");
 		if (user)
 			$(".login_reg").html(`<a href="${user}"></a>`);
 	});
-
+	//加载尾部
 	$.get("/html/include/footer.html", function(data){
 		$("footer").append(data);
+	});
+	//加载aside
+	$.get("/html/include/aside.html", function(data){
+		$("aside").append(data);
 	});
 });
